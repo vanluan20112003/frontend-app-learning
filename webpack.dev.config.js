5546af10bf2c42d59b0495276a24fdb0fa20e173
Luan <luan@openedx.local>
2025-10-05 01:10:13 +0000

    fix: Suppress Sass deprecation warnings in webpack build
    - Add quietDeps and verbose:false to sass-loader options
    - Suppress Sass @import deprecation warnings
    - Suppress legacy JS API warnings
    - Suppress global built-in function warnings
    - Apply to both dev and prod webpack configs
    
    Also includes:
    - package-lock.json with locked dependency versions
    - html-webpack-plugin added as explicit dev dependency
    
    These warnings are non-critical deprecation notices that don't
    affect functionality but clutter the build output.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -8,4 +8,23 @@ config.resolve.alias = {
   '@src': path.resolve(__dirname, 'src'),
 };
 
+// Suppress Sass deprecation warnings
+config.module.rules.forEach((rule) => {
+  if (rule.test && rule.test.toString().includes('scss')) {
+    rule.use.forEach((loader) => {
+      if (loader.loader && loader.loader.includes('sass-loader')) {
+        loader.options = {
+          ...loader.options,
+          sassOptions: {
+            ...loader.options?.sassOptions,
+            quietDeps: true, // Suppress deprecation warnings from dependencies
+            verbose: false,
+          },
+          warnRuleAsWarning: false, // Don't show sass warnings as webpack warnings
+        };
+      }
+    });
+  }
+});
+
 module.exports = config;
